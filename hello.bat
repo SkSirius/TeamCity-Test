4cc3e073d15827ea92bc6f01153f3692f869ccb2
Pavlo Sobchuk <Pavlo Sobchuk>
2017-03-03 17:27:22 +0200

    'bat'

--- hello.bat
+++ hello.bat
@@ -1,2 +1,19 @@
-echo Hello
-pause
+echo off
+
+echo Do you want to deploy xxxx to DerServ(yn): 
+set /p input=
+
+if "%input%" == "y" goto :1
+if NOT "%input%" == "y" goto :2
+
+:1
+SET MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+%MSBUILD% xxxxx.defaultTeamCity.msbuild /target:projBuild
+
+goto end
+ :2
+ECHO Exiting...
+goto end
+ :end   
+
+ pause
